fbadd92f7db51fddd3cdf80503e7200956bd146e
fiqmanan <muhammadsyafiqmanan@gmail.com>
2020-04-02 23:42:15 +0800

    second commit

--- polymorphism.java
+++ polymorphism.java
@@ -1,3 +1,22 @@
+/************************************************
+1. *Copyright. For internal Use only
+*
+*FILE: polymorphism.java
+*PROJECT:Problem Solving 3
+*Module:1
+*
+*Description:
+*This is just for example to understand the polymorphism
+*
+*Note:
+*
+*Compiler dependencies or special instruction:
+*This project is run using Textpad and compile using java
+*
+*Revision History
+*Date: 2nd April 2019   By: Muhammad Syafiq Bin Abdul Manan   Description:This is the intial push
+
+*************************************************/
 class Game{
  public void type()
  {  System.out.println( "Indoor & outdoor" ); }
