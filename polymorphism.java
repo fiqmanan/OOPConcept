7d413ec6a1e86d87213673dedf74e8f778159531
fiqmanan <muhammadsyafiqmanan@gmail.com>
2020-04-02 23:19:43 +0800

    InitalCommit

---
+++ polymorphism.java
@@ -0,0 +1,23 @@
+class Game{
+ public void type()
+ {  System.out.println( "Indoor & outdoor" ); }
+}
+
+class Cricket extends Game{
+ public void type()
+ {  System.out.println( "outdoor game" ); }
+
+
+public class polymorphism{
+
+ public static void main(String[] args) {
+   Game gm = new Game();
+   Cricket ck = new Cricket();
+   gm.type();
+   ck.type();
+   gm=ck;                     //gm refers to Cricket object
+   gm.type();               //calls Cricket's version of type
+ }
+}
+
+}
