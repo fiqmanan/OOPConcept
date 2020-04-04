3216e61765e2eb9d8535d7d5dc97b3b959dcbd04
fiqmanan <muhammadsyafiqmanan@gmail.com>
2020-04-04 17:46:32 +0800

    Add Overriding in the methods

--- ProjectCode/polymorphismProject.java
+++ ProjectCode/polymorphismProject.java
@@ -21,9 +21,11 @@
 import java.util.Scanner;
 import java.util.*;
 import java.util.ArrayList;
+import java.io.*;
 
 public class polymorphismProject{
 
+	//----------------Class User---------------------//
 	private static class User{
 		protected String name;
 		protected String userType;
@@ -82,7 +84,7 @@ public class polymorphismProject{
 			        this.password = password;
     			}
 
-    			//Apply polymorphism
+    			//Apply polymorphism Overloading methods
     			public void display(User user){
 					System.out.println(user.name);
 					System.out.println(user.userType);
@@ -90,89 +92,95 @@ public class polymorphismProject{
 					System.out.println(user.username);
 					System.out.println(user.password);
 				}
-			}
 
+				public User RegisterUser(){
+
+						Scanner scan = new Scanner(System.in);
+
+						System.out.println("Name: ");
+						String name =(scan.nextLine());
+
+						System.out.println("User Type: ");
+						String setUserType = (scan.nextLine());
+
+						System.out.println("IC Number: ");
+						long setIcNumber = (Long.parseLong(scan.nextLine()));
+
+						System.out.println("Username: ");
+						String setUsername =(scan.nextLine());
+
+						System.out.println("Password: ");
+						String setPassword = (scan.nextLine());
+
+						User user = new User(name,setUserType,setIcNumber,setUsername,setPassword);
+
+						return user;
+				}
+		}
+
+	//---------------class Doctor----------------------//
 	public static class Doctor extends User{
 		String DoctorName;
 		String RoomNumber;
 
-		public Doctor(){
-		}
+		public Doctor(){}
 
 		public Doctor(String RoomNumber,String doctorName){
 			this.DoctorName = doctorName;
 			this.RoomNumber = RoomNumber;
 		}
-		//Apply polymorphism
+
+		//Apply polymorphism Overloading methods
 		public void display(Doctor doctor){
 
 			System.out.println("\nList Of Doctor\n");
 			System.out.println(doctor.DoctorName);
 			System.out.println(doctor.RoomNumber);
-	}
-	}
-
-	public static User RegisterUser(){
-
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Name: ");
-		String name =(scan.nextLine());
-
-		System.out.println("User Type: ");
-		String setUserType = (scan.nextLine());
-
-		System.out.println("IC Number: ");
-		long setIcNumber = (Long.parseLong(scan.nextLine()));
-
-		System.out.println("Username: ");
-		String setUsername =(scan.nextLine());
-
-		System.out.println("Password: ");
-		String setPassword = (scan.nextLine());
-
-		User user = new User(name,setUserType,setIcNumber,setUsername,setPassword);
-
-		return user;
 		}
 
-	public static Doctor RegisterDoctor(){
-		Scanner scan = new Scanner(System.in);
+		//Apply polymorphism Overriding method to the parent class with different data type
+		public Doctor RegisterUser(){
+			Scanner scan = new Scanner(System.in);
 
-		System.out.println("Doctor Name: ");
-		String setDoctorName =(scan.nextLine());
+			System.out.println("Doctor Name: ");
+			String setDoctorName =(scan.nextLine());
 
-		System.out.println("Doctor Room: ");
-		String setDoctorRoom = (scan.nextLine());
+			System.out.println("Doctor Room: ");
+			String setDoctorRoom = (scan.nextLine());
 
-		Doctor doctor = new Doctor(setDoctorRoom,setDoctorName);
+			Doctor doctor = new Doctor(setDoctorRoom,setDoctorName);
 
-		return doctor;
+			return doctor;
+		}
 	}
 
+	//--------------Main function----------------------//
+
 	public static void main(String[] args){
+
 		ArrayList<User> objUser = new ArrayList<User>();
 		ArrayList<Doctor> objDoc = new ArrayList<Doctor>();
 
 		//Inser some dummy data to do comparision
 		User patient = new User("syafiq","Nurse",123456789012L,"roselia","12345");
+
 		//Add the into ArrayList
 		objUser.add(patient);
+
 		System.out.println("Welcome To Patient Navigation System");
 		Scanner scan = new Scanner(System.in);
 		int answer;
 
 		do{
-		System.out.println("Do you want to add new [1] - user  or [2] - Doctor ");
+		System.out.println("Do you want to add new [1] - Patient  or [2] - Doctor ");
 		answer = Integer.parseInt((scan.nextLine()));
 
 		switch(answer){
 		case 1:
 				do{
 						System.out.println("Add New User");
-						User user = RegisterUser();
-						//objUser.add(user);
-
+						User user = new User();
+						user = user.RegisterUser();
 						user.display(user);
 
 						for(int i =0; i< objUser.size();i++){
@@ -199,18 +207,21 @@ public class polymorphismProject{
 							answer = Integer.parseInt((scan.nextLine()));
 						}while(answer != 0);
 				break;
+
 		case 2:
 						do{
 
 							System.out.println("Add New Doctor");
-							Doctor doctor = RegisterDoctor();
-							objDoc.add(doctor);
+							Doctor doctor = new Doctor();
+							Doctor doctorParam = doctor.RegisterUser();
+							objDoc.add(doctorParam);
 
 							do{
 							System.out.println("Do you want to delete that doctor [1] - yes , [0] - no");
 							answer = Integer.parseInt((scan.nextLine()));
 
 							if(answer == 1){
+								System.out.print("\b \b");
 								for(int i =0; i< objDoc.size();i++){
 									System.out.println("Doctor Index : [" + i +"]");
 									Doctor doc = (Doctor)objDoc.get(i);
